898dfad23490754c058860cc27b455d06516fe27
jamalxwm <jamal@letswayfare.com>
2024-09-25 07:02:36 -0400

    creates genre model

--- genrenaut/genrenaut/genres/models.py
+++ genrenaut/genrenaut/genres/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    display_name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    spotify_playlist_id = models.CharField(max_length=250, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.display_name
